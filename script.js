ca35049d24b1fd8c987bb5d4efdaad1dd6d5464d
kwon <kingcobra4@naver.com>
2024-09-26 15:24:36 +0900

    ^^

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
   // span 요소 노드 가져오기
   const spanEl = document.querySelector("main h2 span");
   // 화면에 표시할 문장 배열
-  const txtArr = ['Web Publisher', 'Front-End Developer', 'Web UI Designer', 'UX Designer', 'Back-End Developer'];
+  const txtArr = ['웹 퍼블리셔', '프론트 엔드 개발자', '웹 UI 디자이너', 'UX 디자이너', '백엔드 개발자'];
   // 배열의 인덱스 초깃값
   let index = 0;
   // 화면에 표시할 문장 배열에서 요소를 하나 가져온 뒤, 배열로 만들기
